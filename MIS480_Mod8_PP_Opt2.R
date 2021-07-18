51f58fedbe61f9a9047e7ea25d045daeff03bb2c
Devin Moya <devin.moya@woz-u.com>
2021-07-18 14:05:23 -0700

    Output chage
    Used .head to shorten output

--- MIS480_Mod8_PP_Opt2.R
+++ MIS480_Mod8_PP_Opt2.R
@@ -8,6 +8,8 @@ data <- read.csv("data.csv")
 features <- data[, c( "acousticness", "danceability", "duration_ms", "energy",          
                         "instrumentalness", "key", "liveness", "loudness", 
                         "mode", "speechiness", "tempo", "valence", 'popularity')]
+
+#create linear regression model                 
 lmMod <- lm(popularity~., data=features)
 summary(lmMod)
 
@@ -15,11 +17,18 @@ summary(lmMod)
 features2 <- features
 features2$popularityABOVE.50 <- ifelse(features2$popularity > 50, 1, 0)
 
+#create logisitic model
 mylogit <- glm(popularityABOVE.50 ~ . - popularity, data=features2, family="binomial")
+
+#create soft probabilities 
 probabilities <- predict(mylogit, type = "response")
+
+#create hard predictions 
 features2$Predicted <- ifelse(probabilities > .5, 1, 0)
 features2$Predicted <- as.factor(features2$Predicted)
 features2$popularityABOVE.50 <- as.factor(features2$popularityABOVE.50)
+
+#create confusion matrix
 conf_mat <- caret::confusionMatrix(features2$Predicted, features2$popularityABOVE.50)
 conf_mat
 
